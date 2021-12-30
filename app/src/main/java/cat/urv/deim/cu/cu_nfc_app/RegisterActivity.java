c83b57179e218f172823226887f016855a35aef0
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2021-12-30 16:28:23 +0100

    coses

---
+++ app/src/main/java/cat/urv/deim/cu/cu_nfc_app/RegisterActivity.java
@@ -0,0 +1,33 @@
+package cat.urv.deim.cu.cu_nfc_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
+        findViewById(R.id.Entrada).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent firestore = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(firestore);
+                finish();
+            }
+        });
+
+    }
+}
